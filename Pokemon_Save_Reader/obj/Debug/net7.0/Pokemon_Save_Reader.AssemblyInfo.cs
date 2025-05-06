370ab682c8f5308ac68354500126ec683e6582b2
Graungher <camermcgill11@yahoo.com>
2025-05-06 10:12:24 -0400

    fixed gitignore?
    added a bunch of stuff to gitignore, hoping it doesn't break anything

--- Pokemon_Save_Reader/obj/Debug/net7.0/Pokemon_Save_Reader.AssemblyInfo.cs
+++ Pokemon_Save_Reader/obj/Debug/net7.0/Pokemon_Save_Reader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon_Save_Reader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c54047ac65d97a8f9980c5d9d5ce65673e42ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon_Save_Reader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon_Save_Reader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
