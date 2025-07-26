d8a8504e0da24c4fa154e074f090c552dd838b3a
Graungher <camermcgill11@yahoo.com>
2025-07-25 22:15:15 -0400

    added options in main
    So Cool!

--- Pokemon_Save_Reader/Program.cs
+++ Pokemon_Save_Reader/Program.cs
@@ -17,8 +17,41 @@ namespace MyApp
                 //String pokemonName;
 
                 PokemonBoxHandler a = new PokemonBoxHandler(fileBytes, GameVersion.Blue);
-                readAllBoxes(a);
+                
+                int i;
+                int boxNo;
+                int monNo;
+                bool quit = false;
+                while (!quit)
+                {
+                    Console.WriteLine("Whatcha wanna do? 1: Read Box   2: Read them all?   3: Look at a guy?   4: Quit?");
+                    int.TryParse(Console.ReadLine(), out i);
 
+                    switch (i)
+                    {
+                        case 1:
+                            Console.WriteLine("Gimme a box number:");
+                            int.TryParse(Console.ReadLine(), out boxNo);
+
+                            ReadBox(boxNo, a);
+                            break;
+                        case 2:
+                            ReadAllBoxes(a);
+                            break;
+                        case 3:
+                            Console.WriteLine("Gimme a box number:");
+                            int.TryParse(Console.ReadLine(), out boxNo);
+
+                            Console.WriteLine("Gimme a man number:");
+                            int.TryParse(Console.ReadLine(), out monNo);
+
+                            ReadPokemonInBox(monNo, boxNo, a);
+                            break;
+                        default:
+                            quit = true;
+                            return;
+                    }
+                }
 
                 // here add option for selecting a specific pokemon then show all details
                 // prob select box
@@ -29,29 +62,37 @@ namespace MyApp
                 Console.WriteLine($"Error reading file: {ex.Message}");
             }
         }
-
-        static void readBox(int boxNumber, PokemonBoxHandler a)
+        static void ReadPokemonInBox(int pokemonIndex, int boxNumber, PokemonBoxHandler a)
+        {
+            String pokemonName;
+            if ((pokemonIndex! > a.BoxOccupantCount(boxNumber)) && pokemonIndex > 0)
+            {
+                Console.WriteLine("Uh Oh, cant do that bucco!");
+                return;
+            }
+            pokemonName = a.GetPokemonNameFromBox(boxNumber, pokemonIndex);
+            Console.WriteLine($"{pokemonIndex,2}: {pokemonName}");
+        }
+        static void ReadBox(int boxNumber, PokemonBoxHandler a)
         {
             int pokemonCount = a.BoxOccupantCount(boxNumber);
-            String pokemonName;
 
             if (pokemonCount != 0)
             {
                 Console.WriteLine($"\nBox: {boxNumber}");
                 for (int pokemonIndex = 1; pokemonIndex <= pokemonCount; pokemonIndex++)
                 {
-                    pokemonName = a.GetPokemonNameFromBox(boxNumber, pokemonIndex);
-                    Console.WriteLine($"{pokemonIndex,2}: {pokemonName}");
+                    ReadPokemonInBox(pokemonIndex, boxNumber, a);
                 }
             }
         }
-        static void readAllBoxes(PokemonBoxHandler a)
+        static void ReadAllBoxes(PokemonBoxHandler a)
         {
             int totalBoxes = a.MaxBoxes();
             Console.Write("This is every Pokemon in the storage");
             for (int boxNumber = 1; boxNumber <= totalBoxes; boxNumber++)
             {
-                readBox(boxNumber, a);
+                ReadBox(boxNumber, a);
             }
         }
     }
